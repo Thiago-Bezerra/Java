f1d040c65ef6766528e345dd47d06549335eee2e
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-01-29 15:44:32 -0300

    tipos de dados, int e double

---
+++ descomplica/aula/Aula03.java
@@ -0,0 +1,10 @@
+
+public class Aula03{
+    public static void main(String[] args){
+        int idade = 10;
+        double peso = 100.65;
+        
+        System.out.println(idade);
+        System.out.println(peso);
+    }
+}
