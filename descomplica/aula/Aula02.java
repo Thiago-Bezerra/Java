c86ff02922cc8fc1946924bb7d6d78f493b4e67f
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-01-29 15:24:56 -0300

    pacote, clsse e metodo, corrigida 01

--- descomplica/aula/Aula02.java
+++ descomplica/aula/Aula02.java
@@ -1,7 +1,7 @@
 package descomplica.aula;
 //pacotes por convecao escreve-se com letras minusculas
 
-public class aula02 {
+public class Aula02 {
     // classe o nome da classe sempre comeca com letra maiscula
     public static void main(String[] args){
         // metodo ou funcao resonsavel pela estrutura do codigo
