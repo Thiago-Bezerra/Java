aa0ef12f1ab62105f6a5619d1cb4aa7df3abd5c3
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-01-29 18:03:42 -0300

    compilando, o erro aconteceu buscando solucoes

--- descomplica/aula/Aula01.java
+++ descomplica/aula/Aula01.java
@@ -1,7 +1,7 @@
 package descomplica.aula;
 
 public class Aula01{
-    public static void helloWorld(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
 }
