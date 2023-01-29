5001afca260e422de38028535ec235d225980e35
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-01-29 17:47:31 -0300

    Saudacao

--- descomplica/aula/Aula01.java
+++ descomplica/aula/Aula01.java
@@ -1,6 +1,7 @@
+package descomplica.aula;
 
 public class Aula01{
     public static void helloWorld(String[] args) {
-        System.out.println("Hello, world!")
+        System.out.println("Hello, world!");
     }
 }
