1c3e2dafb1c5208a93afaf859b20ac660b65bc52
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-01-29 15:23:18 -0300

    pacote, clsse e metodo

---
+++ descomplica/aula/aula02.java
@@ -0,0 +1,14 @@
+package descomplica.aula;
+//pacotes por convecao escreve-se com letras minusculas
+
+public class aula02 {
+    // classe o nome da classe sempre comeca com letra maiscula
+    public static void main(String[] args){
+        // metodo ou funcao resonsavel pela estrutura do codigo
+        int idadeProfessor;
+        //variavel comeca com letra minuscula na primeira palavra e as demais com letra maisculas
+        idadeProfessor = 25;
+        System.out.println(idadeProfessor);
+
+    }
+}
