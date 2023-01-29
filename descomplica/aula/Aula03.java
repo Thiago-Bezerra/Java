9ddb796b6de9db715c532a609fafdd9d90c33fa5
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-01-29 15:53:48 -0300

    tipos de dados, int, double e float, pacote adicinado

--- descomplica/aula/Aula03.java
+++ descomplica/aula/Aula03.java
@@ -1,10 +1,13 @@
+package descomplica.aula;
 
 public class Aula03{
     public static void main(String[] args){
         int idade = 10;
         double peso = 100.65;
+        float altura = 1.7f;
         
         System.out.println(idade);
         System.out.println(peso);
+        System.out.println(altura);
     }
 }
