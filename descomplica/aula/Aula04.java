516f966aa8c8365e009d7247cb328308544d2ee0
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-02-01 21:35:33 -0300

    operadores aritneticos

---
+++ descomplica/aula/Aula04.java
@@ -0,0 +1,13 @@
+package descomplica.aula;
+
+public class Aula04{
+    public static void main(String[] args) {
+        
+        System.out.println(9 + 2);
+        System.out.println(9 - 2);
+        System.out.println(9 * 2);
+        System.out.println(9 / 2);
+        System.out.println(9 % 2);
+
+    }
+}
