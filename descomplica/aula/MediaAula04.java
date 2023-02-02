43a5c4c1446e8ee66d036466b9e059710d0da691
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-02-01 21:53:49 -0300

    operadores aritmeticos, exemplo de media

---
+++ descomplica/aula/MediaAula04.java
@@ -0,0 +1,24 @@
+package descomplica.aula;
+
+import java.util.Scanner;
+
+public class MediaAula04 {
+    public static void main(String[] args){
+
+        int nota1;
+        int nota2;
+
+        Scanner digiteNota = new Scanner(System.in);
+
+        System.out.println("Digite a nota: ");
+        nota1 = digiteNota.nextInt();
+
+        System.out.println("Digite a nota: ");
+        nota2 = digiteNota.nextInt();
+
+        float media = (nota1 + nota2)/2;
+
+        System.out.println("Media = " + media);
+     
+    }
+    }
