c31758aa17ef33421ceed1cc95104a754c17f5c7
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-02-02 20:39:35 -0300

    iniciado o if

---
+++ descomplica/aula/Aula05IfSwitch.java
@@ -0,0 +1,17 @@
+
+public class IfSwitch{
+    public static void main(String[] args) {
+        int valor = 8
+        if(valor == 8){
+            System.out.println("Valor = 8")
+        }
+        else{
+            if(valor > 8){
+                System.out.println("Valor > 8")
+            }
+            else{
+                System.out.println("Valor < 8")
+            }
+        }
+    }
+}
