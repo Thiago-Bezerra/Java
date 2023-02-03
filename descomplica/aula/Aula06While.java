fe79da7111702891f0e021fbbe32b2a019de8707
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-02-03 20:58:15 -0300

    praticando while e doWhile

---
+++ descomplica/aula/Aula06While.java
@@ -0,0 +1,12 @@
+package descomplica.aula;
+
+public class Aula06While {
+    public static void main(String[] args) {
+        int num = 0;
+
+        while(num <= 5){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
