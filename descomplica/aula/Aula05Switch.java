5130db005d855d81eea8cf14d896d93ab359fdc2
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-02-02 21:19:33 -0300

    exemplo de switch

---
+++ descomplica/aula/Aula05Switch.java
@@ -0,0 +1,22 @@
+package descomplica.aula;
+
+public class Aula05Switch {
+    public static void main(String[] args){
+        int num = 2;
+
+        switch(num){
+            case 1:
+                System.out.println("voce digitou 1");
+                break;
+            case 2:
+                System.out.println("voce digitou 2");
+                break;
+            case 3:
+                System.out.println("voce digitou 3");
+                break;
+            default:
+                System.out.println("voce digitou valor invalido.");
+
+        }
+    }
+}
