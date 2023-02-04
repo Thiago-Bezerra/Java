42763af0f41110e019b419257f694c214d8eb737
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-02-03 21:38:54 -0300

    exemplo com o For

---
+++ descomplica/aula/Aula06For.java
@@ -0,0 +1,10 @@
+package descomplica.aula;
+
+public class Aula06For {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
