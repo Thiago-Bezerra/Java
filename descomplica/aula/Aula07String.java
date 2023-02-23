faec1b6ea46384524ea3063ce2b41422434d12d4
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-02-22 23:27:42 -0300

    primaira string

---
+++ descomplica/aula/Aula07String.java
@@ -0,0 +1,16 @@
+package descomplica.aula;
+import java.util.Scanner;
+
+// import java.security.Principal;
+
+public class Aula07String {
+
+    public static void main(String[] args) {
+        
+        String nome;
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Digite o seu nome: ");
+        nome = teclado.next();
+    }
+}
